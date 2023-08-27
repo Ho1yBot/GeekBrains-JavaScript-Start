b23c0a62a21050f31912e36f42b72306006e2887
Svyatoslav <pozolotin-cvat@yandex.ru>
2023-08-27 17:25:50 +0300

    Работа с массивами

--- script.js
+++ script.js
@@ -174,3 +174,81 @@ function allFunc(){
     mult(num1, num2);
     div(num1,num2);
 }
+
+// Задание 1
+// Необходимо с помощью цикла for вывести следующие 11 строк в консоль:
+// 0 – это ноль
+// 1 – нечетное число
+// 2 – четное число
+// 3 – нечетное число
+// …
+// 10 – четное число
+
+function definer(){
+    for (let i = 0; i < 11; i++ ){
+        if (i === 0){
+            console.log(`${i} - это ноль`);
+        }
+        else if (i % 2 === 0) {
+            console.log(`${i} - чётное число`);            
+        }
+        else{
+            console.log(`${i} - нечетное число`);
+        }    
+    }
+}
+
+// Задание 2
+// Дан массив [1, 2, 3, 4, 5, 6, 7]
+// Сделайте из этого массива следующий [1, 2, 3, 6, 7]
+
+function sliceOfArray(){
+    const array = [1, 2, 3, 4, 5, 6, 7];
+    alert(`Исходны массив: ${array}`);
+    array.splice(3,2);
+    alert(`Преобразованный массив: ${array}`); 
+}
+
+// Задание 3
+// Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
+// 1. Рассчитать сумму элементов этого массива
+// 2. Найти минимальное число
+// 3. Найти есть ли в этом массиве число 3
+
+function generateRandomArray() {
+    const length = 5;
+    const randomArray = [];
+    let sum = 0;
+    let min = +Infinity;
+    let count = 0;
+    for (let i = 0; i < length; i++) {
+        const randomNum = Math.floor(Math.random() * 10); // Генерируем случайное число от 0 до 9
+        sum += randomNum;
+        if (randomNum <= min){
+            min = randomNum;
+        }
+        if (randomNum === 3){
+            count++;
+        }
+        randomArray.push(randomNum); // Добавляем число в массив
+    }
+    console.log(`Массив ${randomArray}`);
+    console.log(`Минимальное число: ${min}`);
+    console.log(`Сумма чисел: ${sum}`);
+    console.log(`Всего ${count} шт. троек`);
+}
+
+// *Необязательное задание. *
+// Необходимо вывести горку в консоль (используя цикл for), как показано на рисунке, только у вашей горки должно быть 20 рядов, а не 5:
+
+// x
+// xx
+// xxx
+// xxxx
+// xxxxx
+
+function outputMoiuntain(){
+    for (let i = 1; i <= 20; i++){
+        console.log("x".repeat(i));
+    }
+}
